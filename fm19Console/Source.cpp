12d37c3175f5e9bdff7130294309a2b3e37b51a3
ug-code <ugurgunes@windowslive.com>
2019-02-10 21:12:39 +0300

    Code Optimization

--- fm19Console/Source.cpp
+++ fm19Console/Source.cpp
@@ -3,6 +3,7 @@
 #include "GameVersions.h"
 #include "Helpers.h"
 #include <iomanip>
+#include "service\ServiceClub.h"
 
 
 using namespace std;
@@ -11,42 +12,26 @@ using namespace std;
 unsigned int currentMoney;
 
 //Change team setting
-int newMoney;
+double newMoney;
 
 int main() {
 
-	SetConsoleTitle(L"FM19 REAL EDITOR");
+	SetConsoleTitle("FM19 REAL EDITOR");
 	About();
 	HANDLE phandle = GameLoad(windowName);
 	CurrentClub currentClub;
+	ServiceClub serviceClub;
 
-
-	CurrentMemory clubFullname = FindDmaAddy(phandle, currentClub.information.fullNameAddr, currentClub.information.fullNameOffset, 5);
-	cout << "Current my Team: " << myInt2text(clubFullname.currentValue) << endl;
-
-	CurrentMemory clubReputation = FindDmaAddy(phandle, currentClub.information.reputationAddr, currentClub.information.reputationOffset, 1);
-	short repulationValue = clubReputation.currentValue;
-	cout << "Current my repulation: " << (repulationValue) << endl;
-
-	CurrentMemory clubCompetition = FindDmaAddy(phandle, currentClub.information.competitionAddr, currentClub.information.competitionOffset, 3);
-	cout << "Current my competition: " << myInt2text(clubCompetition.currentValue) << endl;
-
-
-	CurrentMemory clubNation = FindDmaAddy(phandle, currentClub.information.nationAddr, currentClub.information.nationOffset,4 );
-	cout << "Current my nation: " << myInt2text(clubNation.currentValue) << endl;
-
-	CurrentMemory currentMoney = FindDmaAddy(phandle, currentClub.finances.moneyAddr, currentClub.finances.moneyOffsets, 5);
-	cout << "Current money value: " << currentMoney.currentValue << endl;
-	cout << "Please enter an new money value: ";
+	cout << "Current Team                    :" << serviceClub.getClubFullname(phandle) << endl;
+	cout << "Current repulation              :" << serviceClub.getClubReputation(phandle) << endl;
+	cout << "Current competition             :" << serviceClub.getClubCompetition(phandle) << endl;
+	cout << "Current nation                  :" << serviceClub.getClubNation(phandle) << endl;
+	cout << "Current money value             :" << serviceClub.getClubMoney(phandle) << endl;
+	cout << "Please enter an new money value : ";
 	cin >> newMoney;
 
-
-	int isWriteProcess = WritePointer(phandle, currentClub.finances.moneyAddr, newMoney, currentClub.finances.moneyOffsets, 5);
-	if (!isWriteProcess) {
-		std::cerr << "Couldn't write process memory:" << GetLastError() << std::endl;
-		getchar();
-	}
-	cout << "New money value : " << newMoney << endl;
+	serviceClub.setClubMoney(phandle, newMoney);
+	cout << "New money value : " <<fixed << newMoney << endl;
 
 	//Close any handles once the program is over
 	CloseHandle(phandle);
